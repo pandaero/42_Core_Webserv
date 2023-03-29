982908b3748dcc1641ca7b892484639b3084a0ae
wolf <wolf.mardin@gmail.com>
2023-03-29 20:11:52 +0200

    auto add & push

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,11 @@ int main (void)
 {
 	
 	std::string requestString("POST /api/users HTTP/1.1\r\nHost: www.example.com\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36\nContent-Type: application/json\nContent-Length: 44\nConnection: keep-alive\n\n{\"username}\": \"jdoe\", \"password\": \"password123\"}");
+	std::string empty("");
 	Server serverA;
 
+	serverA.parseRequest(empty);
+	serverA.printRequest();
 	serverA.parseRequest(requestString);
 	serverA.printRequest();
 }
